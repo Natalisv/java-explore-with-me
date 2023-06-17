b13184be66cddac5e2678208d7093c5306e01116
Natalisv <nvselina@mail.ru>
2023-06-17 15:32:26 +0300

    Диплом. Вторая часть

--- ewm-service/src/main/java/ru/practicum/service/user/UserServiceImpl.java
+++ ewm-service/src/main/java/ru/practicum/service/user/UserServiceImpl.java
@@ -1,7 +1,6 @@
 package ru.practicum.service.user;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.practicum.model.User;
